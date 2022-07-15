4503b4807152fa04e01a15d72cc57f9c1a7d5821
Valeryia Dalenchuk <valeryia.dalenchuk@innowise-group.com>
2022-07-15 17:30:12 +0300

    add product to cart

--- assets/test.js
+++ assets/test.js
@@ -1,46 +1,23 @@
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
+    initial:0,
+    focusableElements: ['input'],
+    slidesPerPage: 1,
+    slidesPerView: 2,
+    spaceBetween: 12,
     breakpoints: {
-        320:{
-            slidesPerPage: 1,
-            slidesPerView: 1,
-            spaceBetween: 12,
-            loop: false,
-        },
-        375:{
-            slidesPerPage: 1,
-            slidesPerView: 1,
-            spaceBetween: 12,
-            loop: false,
-        },
-        480:{
-            slidesPerPage: 1,
-            slidesPerView: 1,
-            spaceBetween: 12,
-            loop: false,
-        },
-        520:{
-            slidesPerPage: 1,
-            slidesPerView: 1,
-            spaceBetween: 12,
-            loop: false,
-        },
-        600: {
-            slidesPerPage: 2,
-            slidesPerView: 2,
-            spaceBetween: 12,
-            loop: false,
-        },
         900:{
             slidesPerPage: 1,
             slidesPerView: 3,
-            spaceBetween: 20
+            spaceBetween: 20,
+            loop:false
         },
         1230: {
             slidesPerPage: 1,
             slidesPerView: 4,
-            spaceBetween: 24
+            spaceBetween: 24,
+            loop:false
         }
     },
     pagination: {
@@ -53,4 +30,32 @@ const swiper = new Swiper('.swiper', {
     scrollbar: {
         el: '.swiper-scrollbar',
     },
+});
+
+let forms = document.querySelectorAll("form.shopify-product-form");
+[].forEach.call(forms, function (form) {
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let el = event.target;
+        let postObj = {
+            id: el['id'].value,
+            quantity: 1,
+            sections: 'header'
+        };
+        window.fetch(window.Shopify.routes.root + 'cart/add.js', {
+            method: 'post',
+            body: JSON.stringify(postObj),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            return response.json()
+        }).then((res) => {
+            let header = document.getElementById('shopify-section-header');
+            header.innerHTML = res.sections.header;
+        }).catch((error) => {
+            console.log(error)
+        });
+    });
 });
